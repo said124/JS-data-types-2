dcf524670dcd94ff8661190733e67cd65f4fe325
said124 <165512725+said124@users.noreply.github.com>
2025-10-22 09:13:52 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,38 @@
+// console.log('JavaScript работает!');
+
+// let string = 'Hello Tashkent'
+// let number = 18
+// let boolean = true
+// let nul = ''
+// let underfined = ""
+// const object = {
+//     nick: 'Saidovic',
+//     age: 13,
+//     heIsStudentMars: true
+// };
+
+// console.log(typeof 'Hello Tashkent')
+// console.log(typeof 18)
+// console.log(typeof true)
+// console.log(typeof null)
+// console.log(typeof undefined)
+// console.log(typeof {})
+
+
+
+// let text = prompt('Введите имя')
+// let age = 23
+// alert(`Привет, ${text}! Через год тебе будет ${Number(age) +1}.`)
+
+
+
+
+alert('Hozir siz 2 ta xohlagan son yozishingiz kerak boladi.');
+let a = prompt('Birinchi xohlagan soningizni yozing!')
+let b = prompt('Ikkinchi xohlagan soningizni yozing!')
+
+console.log(Number(a)+Number(b))
+console.log(a-b)
+console.log(a*b)
+console.log(a/b)
+alert("HACK WAS SUCCES")
